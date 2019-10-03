4be14112cb8de1df639cc2ac013f0486305d4b21
Scott Stevson <scott@stevson.com>
2019-10-03 12:14:36 -0700

    moving opening braces to same lines as the functions and compount statements

--- printArgs.c
+++ printArgs.c
@@ -8,8 +8,7 @@
 #include <stdio.h>
 
 // Declare the main function with an argument count and argument vector.
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // Declare the 'count' variable.
   int count;
   // Print the single argument with which the user called the program.
@@ -17,17 +16,13 @@ int main(int argc, char *argv[])
 
   // If the argument count is greater than one, increment the count variable
   // to a number equal to all the arguments the user provided.
-  if (argc > 1)
-  {
-    for (count = 1; count < argc; count++)
-    {
+  if (argc > 1) {
+    for (count = 1; count < argc; count++) {
       // And then print the string corresponding to the argument vector
       // position.
       printf("argv[%d] = %s\n", count, argv[count]);
     }
-  }
-  else
-  {
+  } else {
     // Otherwise, let the user know there are no other arguments.
     printf("The command has no other arguments.\n");
   }
