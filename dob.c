cce78e08c21a54ae9fc9a9d4af0872b15f0a39fa
Scott Stevson <scott@stevson.com>
2019-11-15 14:31:30 -0800

    adding some simple exercises from w3resource.com

---
+++ dob.c
@@ -0,0 +1,17 @@
+/*
+ * File: dob.co
+ * ------------
+ *  This program prints my name, DOB, and mobile number to the screen.  It's
+ *  exercise #1 on https://www.w3resource.com/c-programming-exercises webpage.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	printf("Name : Scott Stevson\n"); /* Need to review string formatting */
+	printf("DOB : May 15, 1968\n");   /* and ensure the columns line up.  */
+	printf("Mobile : 206.913.8454\n");
+
+	return 0;
+}
